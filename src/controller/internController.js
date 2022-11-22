da1f14e587eb57e993bfa425a5f9ffbd7a5622e8
sunandasharma61 <sunandasharma60101@gmail.com>
2022-11-22 15:58:50 +0530

    ok

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,5 +1,4 @@
 const internModel = require('../model/internModel')
-
 const collegeModel = require("../model/collegeModel");
 
 
@@ -7,83 +6,83 @@ const collegeModel = require("../model/collegeModel");
 
 
 const createInterns = async function (req, res) {
-    
+
     try {
 
-//....................................................................................................................................................
+        //....................................................................................................................................................
 
         const internData = req.body
         const { name, email, mobile } = internData  //destructuring 
-        
-//.................................................for empty response..................................................................................    
-        
+
+        //.................................................for empty response..................................................................................    
+
         if (Object.keys(internData).length == 0) {
             return res.status(400).send({ status: false, msg: "please enter intern details" })
         }
 
-//..................................................for no name given..................................................................................
+        //..................................................for no name given..................................................................................
 
         if (!name) {
             return res.status(400).send({ status: false, msg: "please provide name" })
         }
 
-//..................................................for name in alphabet only...........................................................................
+        //..................................................for name in alphabet only...........................................................................
 
-        if (!(/^[a-zA-Z ]+$/.test(name))){
-            return res.status(400).send({status:false,msg:"name should be in alphabets"})
+        if (!(/^[a-zA-Z ]+$/.test(name))) {
+            return res.status(400).send({ status: false, msg: "name should be in alphabets" })
         }
 
-//..................................................for no email given..................................................................................
+        //..................................................for no email given..................................................................................
 
         if (!email) {
             return res.status(400).send({ status: false, msg: "please provide email id" })
         }
 
-//..................................................for valid email.....................................................................................
+        //..................................................for valid email.....................................................................................
 
         if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email)) {
             return res.status(400).send({ status: false, msg: "enter valid email" })
         }
 
-//..................................................for same email given................................................................................
-        
-        let checkemail = await internModel.findOne({ email:email })
+        //..................................................for same email given................................................................................
+
+        let checkemail = await internModel.findOne({ email: email })
         if (checkemail) {
             return res.status(400).send({ status: false, msg: "email is already taken" })
         }
 
-//...................................................for no mobile number given..........................................................................
+        //...................................................for no mobile number given..........................................................................
 
         if (!mobile) {
             return res.status(400).send({ status: false, msg: "please provide mobile number" })
         }
 
-//....................................................for same mobile number given........................................................................     
-    
+        //....................................................for same mobile number given........................................................................     
 
-        let mob = await internModel.findOne({mobile:mobile}) 
+
+        let mob = await internModel.findOne({ mobile: mobile })
         if (mob) {
-        return res.status(400).send({status:false,msg:"phone number is already taken"})
+            return res.status(400).send({ status: false, msg: "phone number is already taken" })
         }
 
-// ....................................................for invalid format of number........................................................................
+        // ....................................................for invalid format of number........................................................................
 
         if (!/^(\+\d{1,3}[- ]?)?\d{10}$/.test(mobile)) {
             return res.status(400).send({ status: false, msg: "enter valid mobile number" })
         }
 
-//......................................................for assigning collegeID...............................................................................        
+        //......................................................for assigning collegeID...............................................................................        
 
-        let dataByCollege = await collegeModel.findOne({name:req.body.collegeName})
-        if(!dataByCollege){return res.status(400).send({status:false, message:"There is no intern with this college name"})} 
+        let dataByCollege = await collegeModel.findOne({ name: req.body.collegeName })
+        if (!dataByCollege) { return res.status(400).send({ status: false, message: "There is no intern with this college name" }) }
 
-        req.body.collegeId = dataByCollege._id       
-       
+        req.body.collegeId = dataByCollege._id
+
         let result = await internModel.create(internData)
         return res.status(201).send({ status: true, data: result })
 
-//.....................................................................................................................................    
-    
+        //.....................................................................................................................................    
+
     }
     catch (error) {
         res.status(500).send({ status: false, msg: error.message })
@@ -95,46 +94,45 @@ const createInterns = async function (req, res) {
 
 
 const collegeDetails = async function (req, res) {
-   
-try {
-    
-//..................................................for empty response...................................................................................    
-    
-     const CollegeName = req.query.collegeName;
-     if (!CollegeName) {
-     return res.status(404).send({ status: false, msg: "CollegeName is required" });
-     }
-  
-//...................................................for no data matching..................................................................................
 
-     const collegeData = await collegeModel.findOne({ name: CollegeName })
-     if (collegeData.length == 0) { 
-     return res.status(404).send({ status: false, msg: "No such college are present" })
-     }
-  
-//...................................................for finding interns..................................................................................
+    try {
 
-     let findintern = await internModel.find({ collegeId: collegeData._id })
-      
-     const data = {
-        name: collegeData.name,
-        fullName: collegeData.fullName,
-        logoLink: collegeData.logoLink,
-        interns: findintern
-     }
-      res.status(200).send({ status: true, data: data });
+        //..................................................for empty response...................................................................................    
 
-//.....................................................................................................................................
+        const CollegeName = req.query.collegeName;
+        if (!CollegeName) {
+            return res.status(404).send({ status: false, msg: "CollegeName is required" });
+        }
+
+        //...................................................for no data matching..................................................................................
+
+        const collegeData = await collegeModel.findOne({ name: CollegeName })
+        if (collegeData.length == 0) {
+            return res.status(404).send({ status: false, msg: "No such college are present" })
+        }
+
+        //...................................................for finding interns..................................................................................
+
+        let findintern = await internModel.find({ collegeId: collegeData._id }).select({name:1,email:1,mobile:1,_id:1})
+
+        const data = {
+            name: collegeData.name,
+            fullName: collegeData.fullName,
+            logoLink: collegeData.logoLink,
+            interns: findintern
+        }
+        res.status(200).send({ status: true, data: data });
+
+        //.....................................................................................................................................
 
     }
-     
-    catch (err) 
-    {
-      res.status(500).send({ status: false, msg: err.message });
+
+    catch (err) {
+        res.status(500).send({ status: false, msg: err.message });
     }
 
-  };
-  
-  module.exports = { createInterns, collegeDetails };
+};
+
+module.exports = { createInterns, collegeDetails };
 
 
--- src/router/route.js
+++ src/router/route.js
@@ -1,9 +1,7 @@
 const express = require("express");
-
 const router = express.Router()
 
 const collegeController = require("../controller/collegeController")
-
 const internController = require("../controller/internController")
 
 // <<<<<<<<<<<<<<<<<<<<< Routers >>>>>>>>>>>>>>>>>>>>>>>
