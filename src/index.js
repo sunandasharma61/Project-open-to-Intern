6a2a4ee8f48f15b85ee71eb176d001908fcbce7e
sunandasharma61 <sunandasharma60101@gmail.com>
2022-11-25 14:54:00 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const { default: mongoose }=require('mongoose');
+const multer=require('multer');
 const route=require('./router/route.js');
+
 
 const app=express();
 
 app.use(express.json());
-
+app.use(multer().any());
 
 mongoose.connect("mongodb+srv://project2:VfktOc3jUKliwHi8@cluster1.bil9ljf.mongodb.net/project2", {
     useNewUrlParser: true
@@ -16,6 +18,6 @@ mongoose.connect("mongodb+srv://project2:VfktOc3jUKliwHi8@cluster1.bil9ljf.mongo
 
 app.use('/',route);
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3001))
 });
